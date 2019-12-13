a4a688cfc1bd8bc481a737b4e0af07e1ffd4e2a0
dist0rti0n <senatrom228@gmail.com>
2019-12-14 00:23:42 +0300

    Removed redundant empty lines

--- src/study/java/util/TimeUtil.java
+++ src/study/java/util/TimeUtil.java
@@ -6,6 +6,5 @@ public class TimeUtil {
     public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
-
 }
 
